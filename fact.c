157820421d3c27332a3067edb6384877be274bcd
Snehal Kadam <118713754+snehalkadam-17@users.noreply.github.com>
2022-11-21 12:31:09 +0530

    create fact.c file

---
+++ fact.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+int i,fact=1,n;
+  printf("\n enter n");
+  scanf("%d",&n);
+  
+  
+  for(i=0;i<=n;i++)
+  {
+    fact=fact*1;
+  }
+  println("\n fact=%d",fact);
+}
+     
